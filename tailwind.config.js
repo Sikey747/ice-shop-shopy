57c65eefa6ec8ec2ba56af79eab9846631fa7239
sikey747 <Sikey747@gmail.com>
2024-02-21 17:15:38 +0200

    fix sum problem

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,9 @@ module.exports = {
     colors: {
       primary: "var(--color-primary)",
       "primary-gray1": "var(--primary-gray1)",
+      "primary-gray2": "var(--primary-gray2)",
+      "primary-gray3": "var(--primary-gray3)",
+      "primary-black": "var(--primary-black)",
     },
     extend: {},
   },
